7589a29610586386a4beee55917902949f1aca2a
StefanelaS <103069737+StefanelaS@users.noreply.github.com>
2025-09-15 18:23:59 +0200

    Update app - QC.py

--- app - QC.py
+++ app - QC.py
@@ -42,7 +42,7 @@ def plot_initial_data(initial_data, mean, UAL, UWL, LWL, LAL, x_label, y_label, 
 
     ax.text(x_pos, mean, f' Mean = {mean:.2f}', verticalalignment='bottom', horizontalalignment='right', color='black', 
         bbox=dict(facecolor='none', alpha=0.7, edgecolor='none'))
-    ax.text(x_pos, UAL, f' UAL = {UAL:.2f}', verticalalignment='bottom', color='red',
+    ax.text(x_pos, UAL, f' UAL = {UAL:.2f}', verticalalignment='bottom', horizontalalignment='right', color='red',
         bbox=dict(facecolor='none', alpha=0.7, edgecolor='none'))
     ax.text(x_pos, UWL, f' UWL = {UWL:.2f}', verticalalignment='bottom', color='black',
         bbox=dict(facecolor='none', alpha=0.7, edgecolor='none'))
@@ -53,7 +53,7 @@ def plot_initial_data(initial_data, mean, UAL, UWL, LWL, LAL, x_label, y_label, 
 
     # Set axis limits to create extra space
     ax.set_xticks([])
-    ax.set_xlim(0.5, len(initial_data) + 1.5) 
+    #ax.set_xlim(0.5, len(initial_data) + 1.5) 
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
     ax.set_title(title)
@@ -138,6 +138,7 @@ if new_file:
     new_data = df2.iloc[:, 0].dropna().values
     fig2 = plot_new_data(new_data, initial_data, mean, UAL, UWL, LWL, LAL, x_label, y_label, title)
     st.pyplot(fig2)
+
 
 
 
