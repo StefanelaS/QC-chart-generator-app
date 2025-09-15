bad3a6a07caa7117e0f6518b497870bf5faf4b4c
StefanelaS <103069737+StefanelaS@users.noreply.github.com>
2025-09-15 18:25:32 +0200

    Update app - QC.py

--- app - QC.py
+++ app - QC.py
@@ -53,7 +53,7 @@ def plot_initial_data(initial_data, mean, UAL, UWL, LWL, LAL, x_label, y_label, 
 
     # Set axis limits to create extra space
     ax.set_xticks([])
-    #ax.set_xlim(0.5, len(initial_data) + 1.5) 
+    ax.set_xlim(0.5, len(initial_data) + 1.5) 
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
     ax.set_title(title)
@@ -138,6 +138,7 @@ if new_file:
     new_data = df2.iloc[:, 0].dropna().values
     fig2 = plot_new_data(new_data, initial_data, mean, UAL, UWL, LWL, LAL, x_label, y_label, title)
     st.pyplot(fig2)
+
 
 
 
